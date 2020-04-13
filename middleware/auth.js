773968cee1d65dbb556ecbbbd8c9a5c2c43ef657
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 21:44:32 +0800

    Remove these bullshits

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,14 +5,6 @@ export default async function({ app, store, redirect }) {
     localStorage.getItem('school_refresh_token') !== null
   ) {
     app.$axios.setToken(localStorage.getItem('school_access_token'), 'Bearer')
-    app.$axios.setHeader(
-      'Authorization',
-      'Bearer ' + localStorage.getItem('school_access_token')
-    )
-    app.$auth.ctx.app.$axios.setHeader(
-      'Authorization',
-      'Bearer ' + localStorage.getItem('school_access_token')
-    )
   }
 
   // Redirect to login page if there's no token
