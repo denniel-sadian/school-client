152fdb1fa74fd72e68e093a586523132b44df822
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 21:55:25 +0800

    Do this default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,167 @@
 <template>
   <div>
+    <nav>
+      <a href="#">
+        <div class="main">
+          <div id="user-img"></div>
+          <span>Denniel luis Sadian</span>
+        </div>
+      </a>
+      <div class="toggle" @click="showLinks = !showLinks"></div>
+      <div id="links" :class="{ 'w3-show': showLinks }" class="w3-animate-top">
+        <a href="#">Departments</a>
+        <a href="#">Sections</a>
+        <a href="#">Students</a>
+        <a href="#">Permissions</a>
+        <a href="#">Grading Sheets</a>
+        <a @click="logout()">Logout</a>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
 
 <script>
 export default {
-  middleware: 'auth'
+  middleware: 'auth',
+  data() {
+    return {
+      showLinks: true
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.clear()
+      this.$store.commit('user/SET_USER', {})
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+html,
+body,
+#__nuxt,
+#__layout,
+#__layout > div {
+  height: unset;
+  padding: unset;
+  margin: unset;
+}
+</style>
+
+<style scoped>
+body {
+  padding-top: 53px;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+nav {
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  padding: 8px;
+  background: #f1f1f1;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+nav a {
   text-decoration: none;
-  padding: 10px 30px;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+nav .main {
+  display: flex;
+  align-items: center;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+nav .main #user-img {
+  min-width: 40px;
+  min-height: 40px;
+  border-radius: 100%;
+  background: #9e9e9e;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+nav .main span {
+  padding: 0px 18px 0px 8px;
+}
+
+nav > div:last-child {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+}
+
+nav > div:last-child > a {
+  padding: 0px 16px;
+  margin: 0px 4px;
+  border: 1px solid #f1f1f1;
+  color: #9e9e9e;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  border-radius: 32px;
+}
+
+nav > div:last-child > a:hover {
+  border: 1px solid #9e9e9e;
+  background: #fff;
+  color: black;
+  transition: 0.3s;
+}
+
+nav .toggle {
+  width: 40px;
+  height: 40px;
+  background: #f1f1f1;
+  border: 1px solid #9e9e9e;
+  border-radius: 100%;
+  display: none;
+  cursor: pointer;
+}
+
+@media (max-width: 900px) {
+  nav {
+    display: flex;
+    flex-direction: column;
+    padding: 0px;
+  }
+
+  nav .main {
+    display: inline-flex;
+    margin: 8px 0px 0px 8px;
+  }
+
+  nav .toggle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 8px;
+    right: 8px;
+  }
+
+  nav > div:last-child {
+    flex-direction: column;
+    display: none;
+  }
+
+  nav > div:last-child > a {
+    margin: 0px;
+    height: 30px;
+    background: #f1f1f1;
+    border: none;
+    border-radius: unset;
+    width: 100%;
+    height: 40px;
+    justify-content: center;
+    color: black;
+    border: 1px solid #f1f1f1;
+  }
+
+  nav > div:last-child > a:hover {
+    background: #fff;
+  }
 }
 </style>
