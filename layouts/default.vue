e3ce3d026aae19ba8c6f88994d6bcfd3f5b84026
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 22:50:57 +0800

    Make this simple

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,9 +31,7 @@ export default {
   },
   methods: {
     logout() {
-      localStorage.clear()
-      this.$store.commit('user/SET_USER', {})
-      this.$router.push('/login')
+      this.$store.dispatch('user/logout')
     }
   }
 }
