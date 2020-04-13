d4b193f7454b2aef16c37dc1781c25a0e6ee528d
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 23:29:12 +0800

    Make this like this for now

--- pages/registration/check-code.vue
+++ pages/registration/check-code.vue
@@ -1,11 +1,101 @@
 <template>
-  <header></header>
+  <header>
+    <header>
+      <div>
+        <h2>Permission</h2>
+        <p class="w3-small w3-center">
+          Please, enter the account registration permission code that your admin
+          gave you.
+        </p>
+        <hr />
+        <div class="inpt">
+          <label for="code">Permission Code:</label>
+          <input type="text" v-model="code" @keypress.enter="checkCode" />
+        </div>
+        <p v-show="checking" class="w3-small w3-center w3-text-green">
+          Checking code...
+        </p>
+        <p v-show="noCode" class="w3-small w3-center w3-text-red">
+          There is no permission with this code.
+        </p>
+        <hr />
+        <button @click="checkCode" class="w3-button w3-light-green w3-round">
+          Check Code
+        </button>
+      </div>
+    </header>
+  </header>
 </template>
 
 <script>
 export default {
-  layout: 'full_height_body'
+  layout: 'full_height_body',
+  data() {
+    return {
+      code: '',
+      noCode: false,
+      checking: false
+    }
+  },
+  methods: {
+    checkCode() {}
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+html,
+body {
+  height: 100vh;
+  padding: 0px;
+  margin: 0px;
+}
+
+header {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+}
+
+header > div {
+  border: 1px solid #9e9e9e;
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 4px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 4px;
+  max-width: 400px;
+  width: 100%;
+  padding: 16px;
+}
+
+header h2 {
+  text-align: center;
+}
+
+.inpt {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 16px;
+}
+
+.inpt label {
+  font-size: 12px;
+  font-weight: bold;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.inpt input {
+  border-radius: 4px;
+  border: 1px solid gray;
+  height: 40px;
+  transition: 0.3s;
+  padding: 0px 8px;
+}
+
+.w3-button {
+  width: 100%;
+}
+</style>
