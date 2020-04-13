0de97c457225be1e9fbb74549d8b0295cbcfe9cf
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 19:51:04 +0800

    Do the login page

--- layouts/full_height_body.vue
+++ layouts/full_height_body.vue
@@ -4,79 +4,14 @@
   </div>
 </template>
 
-<style scoped>
+<style>
 html,
-body {
+body,
+#__nuxt,
+#__layout,
+#__layout > div {
   height: 100vh;
   padding: 0px;
   margin: 0px;
-}
-
-header {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 16px;
-}
-
-header > div {
-  border: 1px solid #9e9e9e;
-  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 4px 20px 0 rgba(0, 0, 0, 0.19);
-  border-radius: 4px;
-  max-width: 400px;
-  width: 100%;
-  padding: 16px;
-}
-
-header h2 {
-  text-align: center;
-}
-
-.inpt {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-bottom: 16px;
-}
-
-.inpt label {
-  font-size: 12px;
-  font-weight: bold;
-  font-family: 'Courier New', Courier, monospace;
-}
-
-.inpt input {
-  border-radius: 4px;
-  border: 1px solid gray;
-  height: 40px;
-  transition: 0.3s;
-  padding: 0px 8px;
-}
-
-.btn {
-  text-decoration: none;
-  padding: 6px 8px;
-  border-radius: 4px;
-  display: block;
-  text-align: center;
-}
-
-.or {
-  display: flex;
-  align-items: center;
-  padding: 8px 0px;
-}
-
-.or hr {
-  width: 100%;
-  margin: 0px;
-  border: 0.5px solid gray;
-}
-
-.or span {
-  padding: 0px 16px;
 }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,29 @@
 <template>
-  <div></div>
+  <header>
+    <div>
+      <h2>Credentials</h2>
+      <p class="w3-small w3-center">Please, enter your credentials to login.</p>
+      <hr />
+      <div class="inpt">
+        <label for="username">Username:</label>
+        <input type="text" name="username" />
+      </div>
+      <div class="inpt">
+        <label for="password">Password:</label>
+        <input type="password" name="password" />
+      </div>
+      <p class="w3-small w3-text-green w3-center">Logging in...</p>
+      <p class="w3-small w3-text-red w3-center">Wrong credentials</p>
+      <hr />
+      <button class="w3-button w3-light-green">Login</button>
+      <div class="or">
+        <hr />
+        <span>Or</span>
+        <hr />
+      </div>
+      <button class="w3-button w3-light-blue">Register</button>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -8,4 +32,69 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+header {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+}
+
+header > div {
+  border: 1px solid #9e9e9e;
+  box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.2), 0 4px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 4px;
+  max-width: 400px;
+  width: 100%;
+  padding: 16px;
+}
+
+header h2 {
+  text-align: center;
+}
+
+.inpt {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 16px;
+}
+
+.inpt label {
+  font-size: 12px;
+  font-weight: bold;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.inpt input {
+  border-radius: 4px;
+  border: 1px solid gray;
+  height: 40px;
+  transition: 0.3s;
+  padding: 0px 8px;
+}
+
+.w3-button {
+  width: 100%;
+  border-radius: 4px;
+}
+
+.or {
+  display: flex;
+  align-items: center;
+  padding: 8px 0px;
+}
+
+.or hr {
+  width: 100%;
+  margin: 0px;
+  border: 0.5px solid gray;
+}
+
+.or span {
+  padding: 0px 16px;
+}
+</style>
