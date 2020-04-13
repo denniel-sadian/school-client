11464f11283e20f83c7039751235674de46a3dab
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-13 21:41:32 +0800

    Add this new plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/axios'],
+  plugins: ['~/plugins/axios', '~/plugins/refresh_token.js'],
   /*
    ** Nuxt.js dev-modules
    */
---
+++ plugins/refresh_token.js
@@ -0,0 +1,19 @@
+export default function({ $axios, redirect }) {
+  const refresher = () => {
+    const rToken = localStorage.getItem('school_refresh_token')
+
+    if (rToken === null) return
+
+    console.log('Refreshing the token.')
+
+    $axios
+      .post('accounts/refresh-token/', {
+        refresh: rToken
+      })
+      .then((res) => {
+        localStorage.setItem('school_access_token', res.data.access)
+        $axios.setToken(res.data.access, 'Bearer')
+      })
+  }
+  setInterval(refresher, 10000)
+}
