a999c098c4787c9632e19ed889aa5d8bd928dba8
Denniel <sadiandenniel@gmail.com>
2020-04-14 19:53:52 +0800

    Apply the token

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,7 @@
 export default function({ $axios, redirect }) {
+  const t = localStorage.getItem('school_access_token')
+  if (t !== null) $axios.setToken(t, 'Bearer')
+
   $axios.onRequest((config) => {
     console.log('Making request to ' + config.url)
   })
