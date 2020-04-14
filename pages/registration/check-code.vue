f2c56515fc28e74eccfc179189661d935d27278d
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-14 10:48:09 +0800

    Delete redundant tag

--- pages/registration/check-code.vue
+++ pages/registration/check-code.vue
@@ -1,41 +1,39 @@
 <template>
   <header>
-    <header>
-      <div>
-        <h2>Permission</h2>
-        <p class="w3-small w3-center">
-          Please, enter the account registration permission code that your admin
-          gave you.
-        </p>
-        <hr />
-        <div class="inpt">
-          <label for="code">Permission Code:</label>
-          <input type="text" v-model="code" @keypress.enter="checkCode" />
-        </div>
-        <p v-show="checking" class="w3-small w3-center w3-text-green">
-          Checking code...
-        </p>
-        <p v-show="noCode" class="w3-small w3-center w3-text-red">
-          There is no permission with this code.
-        </p>
-        <hr />
-        <button @click="checkCode" class="w3-button w3-light-green">
-          Check Code
-        </button>
-        <div class="or">
-          <hr />
-          <span>Or</span>
-          <hr />
-        </div>
-        <nuxt-link
-          to="/login"
-          class="w3-button w3-light-blue"
-          :disabled="disabled"
-        >
-          Login
-        </nuxt-link>
+    <div>
+      <h2>Permission</h2>
+      <p class="w3-small w3-center">
+        Please, enter the account registration permission code that your admin
+        gave you.
+      </p>
+      <hr />
+      <div class="inpt">
+        <label for="code">Permission Code:</label>
+        <input type="text" v-model="code" @keypress.enter="checkCode" />
       </div>
-    </header>
+      <p v-show="checking" class="w3-small w3-center w3-text-green">
+        Checking code...
+      </p>
+      <p v-show="noCode" class="w3-small w3-center w3-text-red">
+        There is no permission with this code.
+      </p>
+      <hr />
+      <button @click="checkCode" class="w3-button w3-light-green">
+        Check Code
+      </button>
+      <div class="or">
+        <hr />
+        <span>Or</span>
+        <hr />
+      </div>
+      <nuxt-link
+        to="/login"
+        class="w3-button w3-light-blue"
+        :disabled="disabled"
+      >
+        Login
+      </nuxt-link>
+    </div>
   </header>
 </template>
 
