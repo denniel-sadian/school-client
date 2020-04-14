38dad43e1639b6643991682a3a8666368e93a4ee
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-14 10:40:56 +0800

    Move some css

--- layouts/full_height_body.vue
+++ layouts/full_height_body.vue
@@ -14,4 +14,46 @@ body,
   padding: 0px;
   margin: 0px;
 }
+.inpt {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-bottom: 16px;
+}
+
+.inpt label {
+  font-size: 12px;
+  font-weight: bold;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.inpt input {
+  border-radius: 4px;
+  border: 1px solid gray;
+  height: 40px;
+  transition: 0.3s;
+  padding: 0px 8px;
+}
+
+.w3-button {
+  width: 100%;
+  border-radius: 4px;
+}
+
+.or {
+  display: flex;
+  align-items: center;
+  padding: 8px 0px;
+}
+
+.or hr {
+  width: 100%;
+  margin: 0px;
+  border: 0.5px solid gray;
+}
+
+.or span {
+  padding: 0px 16px;
+}
 </style>
--- pages/registration/check-code.vue
+++ pages/registration/check-code.vue
@@ -19,7 +19,7 @@
           There is no permission with this code.
         </p>
         <hr />
-        <button @click="checkCode" class="w3-button w3-light-green w3-round">
+        <button @click="checkCode" class="w3-button w3-light-green">
           Check Code
         </button>
         <div class="or">
@@ -29,7 +29,7 @@
         </div>
         <nuxt-link
           to="/login"
-          class="w3-button w3-light-blue w3-round"
+          class="w3-button w3-light-blue"
           :disabled="disabled"
         >
           Login
@@ -101,47 +101,5 @@ header > div {
 
 header h2 {
   text-align: center;
-}
-
-.inpt {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  margin-bottom: 16px;
-}
-
-.inpt label {
-  font-size: 12px;
-  font-weight: bold;
-  font-family: 'Courier New', Courier, monospace;
-}
-
-.inpt input {
-  border-radius: 4px;
-  border: 1px solid gray;
-  height: 40px;
-  transition: 0.3s;
-  padding: 0px 8px;
-}
-
-.w3-button {
-  width: 100%;
-}
-
-.or {
-  display: flex;
-  align-items: center;
-  padding: 8px 0px;
-}
-
-.or hr {
-  width: 100%;
-  margin: 0px;
-  border: 0.5px solid gray;
-}
-
-.or span {
-  padding: 0px 16px;
 }
 </style>
