378c4c60ba7331d7a0c6752b059c8047a3b71863
Denniel <sadiandenniel@gmail.com>
2020-04-15 13:47:08 +0800

    Make this timeout 1 minute

--- plugins/refresh_token.js
+++ plugins/refresh_token.js
@@ -13,5 +13,5 @@ export default function({ $axios, redirect }) {
         $axios.setToken(res.data.access, 'Bearer')
       })
   }
-  setInterval(refresher, 30000)
+  setInterval(refresher, 60000)
 }
