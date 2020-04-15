98502de739798b49f12297a14036eb14b08e2118
Denniel <sadiandenniel@gmail.com>
2020-04-15 16:42:11 +0800

    Create this vue

---
+++ pages/registration-permissions.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <h1>Account Registration Permissions</h1>
+      <p>
+        This page can only be accessed by the admins. Before anyone creates an
+        account, an admin must provide a permission first.
+      </p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+header {
+  margin-top: 53px;
+  padding: 64px 0px !important;
+  text-align: center;
+}
+
+header h1 {
+  text-transform: capitalize;
+}
+</style>
