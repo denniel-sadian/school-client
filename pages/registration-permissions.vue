fb0819392ba4271d5deec670f24c260ae1759e9d
Denniel <sadiandenniel@gmail.com>
2020-04-15 17:36:59 +0800

    Add this

--- pages/registration-permissions.vue
+++ pages/registration-permissions.vue
@@ -8,23 +8,39 @@
       </p>
     </header>
     <article class="w3-container">
-      <div class="w3-content">
-        {{ permissions }}
+      <p v-if="loadingPerms" class="w3-center">
+        <i class="fas fa-spinner w3-spin"></i>
+      </p>
+      <div v-else class="w3-content">
+        <div v-if="permissions.length === 0" class="w3-center">
+          <h4>There are no permissions yet.</h4>
+        </div>
+        <div v-else>
+          <RegPerm v-for="perm in permissions" :perm="perm" :key="perm.id" />
+        </div>
       </div>
     </article>
   </div>
 </template>
 
 <script>
+import RegPerm from '~/components/RegPerm.vue'
+
 export default {
   middleware: 'isAdmin',
+  components: { RegPerm },
+  data() {
+    return { loadingPerms: true }
+  },
   computed: {
     permissions() {
       return this.$store.state.user.permissions
     }
   },
-  mounted() {
-    this.$store.dispatch('user/getPerms')
+  async mounted() {
+    await this.$store.dispatch('user/getPerms').then(() => {
+      this.loadingPerms = false
+    })
   }
 }
 </script>
