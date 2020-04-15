56b15041568a0680e784b32ba53afe6bca9c2f93
Denniel <sadiandenniel@gmail.com>
2020-04-15 17:09:10 +0800

    Ok

---
+++ pages/not-allowed.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header>
+      <h1>
+        <i class="fas fa-exclamation-triangle w3-text-red"></i> Not Allowed!
+      </h1>
+      <p>You are not an admin to view that page.</p>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+header {
+  margin-top: 53px;
+  padding: 64px 0px !important;
+  text-align: center;
+}
+
+header h1 {
+  text-transform: capitalize;
+}
+</style>
