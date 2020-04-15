8450d1c4cda293c310cff050a66bc93943b161dc
Denniel <sadiandenniel@gmail.com>
2020-04-15 20:41:10 +0800

    Ok

--- components/RegPerm.vue
+++ components/RegPerm.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="cont">
     <div class="display">
-      <h3 :class="{ 'w3-text-green': perm.used }">{{ perm.code }}</h3>
+      <h3 :class="{ 'w3-text-green': perm.used }">
+        <i class="fas fa-key"></i> {{ perm.code }}
+      </h3>
       <p>
         This permission was given to
-        {{ perm.gender === 'm' ? 'Mister' : 'Miss' }} {{ perm.first_name }}
-        {{ perm.last_name }} by {{ perm.from_who.first_name }}
-        {{ perm.from_who.last_name }} to create
+        <b
+          >{{ perm.gender === 'm' ? 'Mister' : 'Miss' }} {{ perm.first_name }}
+          {{ perm.last_name }}</b
+        >
+        by {{ perm.from_who.first_name }} {{ perm.from_who.last_name }} to
+        create
         {{ perm.role === 'admin' ? "an admin's" : "a teacher's" }} account on
         the system.
         <span v-show="perm.used" class="w3-opacity"
@@ -14,6 +19,11 @@
         >
       </p>
       <p class="w3-small">{{ new Date(perm.date).toDateString() }}</p>
+      <div class="btn" v-show="perm.from_who.username === username">
+        <button class="w3-button w3-round w3-small w3-border w3-border-black">
+          <i class="fas fa-pencil-alt"></i>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -22,6 +32,16 @@
 export default {
   props: {
     perm: Object
+  },
+  data() {
+    return {
+      editing: false
+    }
+  },
+  computed: {
+    username() {
+      return this.$store.state.user.user.user.username
+    }
   }
 }
 </script>
@@ -36,5 +56,19 @@ export default {
 
 h3 {
   font-weight: 800;
+}
+
+.btn {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.btn button {
+  width: 34px;
+  height: 34px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
 }
 </style>
