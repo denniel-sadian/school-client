c0a2a03e2a0b8b8eafd8b122e65a819eff83760c
Denniel <sadiandenniel@gmail.com>
2020-04-17 13:11:57 +0800

    Ok

---
+++ pages/permissions.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+    <header class="w3-container">
+      <div class="w3-content w3-padding w3-center">
+        <h1>The Permissions For Viewing The Grading Sheets</h1>
+        <p>
+          Both teachers and admins can view this page, but only the admins can
+          modify the permissions.
+        </p>
+      </div>
+    </header>
+    <article class="w3-container" v-show="role === 'admin'">
+      <form @submit.prevent="createPerm" class="w3-content">
+        <h2><i class="fas fa-plus-circle"></i> Add a Permission</h2>
+        <div class="inpt">
+          <label>Permission Code:</label>
+          <input type="text" v-model="code" required :disabled="creating" />
+        </div>
+        <div class="inpt">
+          <label>For Section:</label>
+          <select v-model="dep" required>
+            <option v-for="dep in departments" :value="dep.url" :key="dep.id">{{
+              dep.name
+            }}</option>
+          </select>
+        </div>
+        <hr />
+        <p class="w3-center w3-text-red w3-small" v-show="error">
+          Please provide a unique name.
+        </p>
+        <button
+          type="submit"
+          :disabled="creating"
+          class="w3-button w3-green w3-round"
+        >
+          <span v-if="creating"
+            ><i class="fas fa-spinner w3-spin"></i> Adding...</span
+          >
+          <span v-else>Add This Section</span>
+        </button>
+      </form>
+    </article>
+    <article class="w3-container">
+      <div class="w3-content">
+        <div v-if="sections.length === 0" class="w3-center">
+          <h4>There is no section yet.</h4>
+        </div>
+        <div v-else>
+          <h2 class="w3-center">List of Sections</h2>
+          <Section
+            v-for="sec in sections"
+            :sec="sec"
+            :deps="departments"
+            :role="role"
+            :key="sec.id"
+          />
+        </div>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+import Section from '~/components/Section.vue'
+
+export default {
+  components: {
+    Section
+  },
+  data() {
+    return {
+      creating: false,
+      error: false,
+      name: '',
+      dep: ''
+    }
+  },
+  computed: {
+    departments() {
+      return this.$store.state.information.departments
+    },
+    sections() {
+      return this.$store.state.information.sections
+    },
+    role() {
+      return this.$store.state.user.user.profile.role
+    }
+  },
+  methods: {
+    async createSection() {
+      this.creating = true
+      this.error = false
+      await this.$axios
+        .post('information/sections/', {
+          name: this.name,
+          department: this.dep
+        })
+        .then(({ data }) => {
+          this.name = ''
+          this.dep = ''
+          this.error = false
+          this.$store.commit('information/PUSH_SECTION', data)
+        })
+        .catch(() => {
+          this.error = true
+          setTimeout(() => {
+            this.error = false
+          }, 10000)
+        })
+        .finally(() => {
+          this.creating = false
+        })
+    }
+  },
+  async mounted() {
+    await this.$store.dispatch('information/getSections')
+  }
+}
+</script>
+
+<style scoped>
+header {
+  margin-top: 53px;
+  padding: 64px 0px !important;
+  text-align: center;
+}
+
+header h1 {
+  text-transform: capitalize;
+}
+
+.w3-content {
+  max-width: 600px;
+}
+
+form {
+  padding: 16px;
+  border-radius: 4px;
+  border: 1px solid black;
+}
+
+.w3-button {
+  width: 100%;
+}
+
+article {
+  margin: 20px 0px;
+}
+</style>
