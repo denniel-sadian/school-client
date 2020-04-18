d345c8bf93c74058e1b3de10711ad15635734d6e
Denniel <sadiandenniel@gmail.com>
2020-04-18 14:53:45 +0800

    Add loading here

--- pages/departments.vue
+++ pages/departments.vue
@@ -11,45 +11,52 @@
         </p>
       </div>
     </header>
-    <article class="w3-container" v-show="role === 'admin'">
-      <form @submit.prevent="createDepartment" class="w3-content">
-        <h2><i class="fas fa-plus-circle"></i> Add a Department</h2>
-        <div class="inpt">
-          <label>Department Name:</label>
-          <input type="text" v-model="name" required :disabled="creating" />
-        </div>
-        <hr />
-        <p class="w3-center w3-text-red w3-small" v-show="error">
-          Please provide a unique name.
-        </p>
-        <button
-          type="submit"
-          :disabled="creating"
-          class="w3-button w3-green w3-round"
-        >
-          <span v-if="creating"
-            ><i class="fas fa-spinner w3-spin"></i> Adding...</span
+    <div v-if="got < 1">
+      <p class="w3-large w3-text-green w3-center">
+        <i class="fas fa-spinner w3-spin"></i> Loading...
+      </p>
+    </div>
+    <div v-else>
+      <article class="w3-container" v-show="role === 'admin'">
+        <form @submit.prevent="createDepartment" class="w3-content">
+          <h2><i class="fas fa-plus-circle"></i> Add a Department</h2>
+          <div class="inpt">
+            <label>Department Name:</label>
+            <input type="text" v-model="name" required :disabled="creating" />
+          </div>
+          <hr />
+          <p class="w3-center w3-text-red w3-small" v-show="error">
+            Please provide a unique name.
+          </p>
+          <button
+            type="submit"
+            :disabled="creating"
+            class="w3-button w3-green w3-round"
           >
-          <span v-else>Add This Department</span>
-        </button>
-      </form>
-    </article>
-    <article class="w3-container">
-      <div class="w3-content">
-        <div v-if="departments.length === 0" class="w3-center">
-          <h4>There is no department yet.</h4>
+            <span v-if="creating"
+              ><i class="fas fa-spinner w3-spin"></i> Adding...</span
+            >
+            <span v-else>Add This Department</span>
+          </button>
+        </form>
+      </article>
+      <article class="w3-container">
+        <div class="w3-content">
+          <div v-if="departments.length === 0" class="w3-center">
+            <h4>There is no department yet.</h4>
+          </div>
+          <div v-else>
+            <h2 class="w3-center">List of Departments</h2>
+            <Department
+              v-for="dep in departments"
+              :dep="dep"
+              :role="role"
+              :key="dep.id"
+            />
+          </div>
         </div>
-        <div v-else>
-          <h2 class="w3-center">List of Departments</h2>
-          <Department
-            v-for="dep in departments"
-            :dep="dep"
-            :role="role"
-            :key="dep.id"
-          />
-        </div>
-      </div>
-    </article>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -62,6 +69,7 @@ export default {
   },
   data() {
     return {
+      got: 0,
       creating: false,
       error: false,
       name: ''
@@ -98,7 +106,9 @@ export default {
     }
   },
   async mounted() {
-    await this.$store.dispatch('information/getDepartments')
+    await this.$store
+      .dispatch('information/getDepartments')
+      .then(() => this.got++)
   }
 }
 </script>
