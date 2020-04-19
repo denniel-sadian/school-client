3da6d35b1043f7f068461c24090528c41afc5aa1
Dein <sadiandenniel@gmail.com>
2020-04-19 16:16:44 +0800

    Create a new component

---
+++ components/Work.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h4>{{ work.name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    work
+  },
+  computed: {
+    workType() {
+      switch (this.work.work_type) {
+        case 'a':
+          return 'Activity'
+        case 'q':
+          return 'Quiz'
+        case 'e':
+          return 'Examination'
+        case 'p':
+          return 'Performace'
+        case 'c':
+          return 'Extra'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  font-family: 'Nunito';
+}
+</style>
