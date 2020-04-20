337638824d95b1695f5ced2d0f8d61dcd6f317e4
Dein <sadiandenniel@gmail.com>
2020-04-20 17:27:41 +0800

    Create a new component

---
+++ components/Col.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <td>{{ record.score }}</td>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    record: Object
+  }
+}
+</script>
+
+<style scoped></style>
