745bc4bd622d6b9bd643aa7ed05bd5aff784d619
Dein <sadiandenniel@gmail.com>
2020-04-22 21:08:29 +0800

    Chnage this

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: 'School',
+    title: 'Online Grading Sheets',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Do your grading sheets online!'
       }
     ],
     link: [
