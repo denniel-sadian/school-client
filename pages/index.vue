8b136d7903f87f8f3d5b7c5efebd9017440329cc
Dein <sadiandenniel@gmail.com>
2020-04-28 11:08:58 +0800

    Create this very index

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cont">
+    <div>
+      <h1>Welcome to the Grading Sheet System</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cont {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto 1fr;
+}
+</style>
