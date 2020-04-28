f66face46f0f60028be89f1ecb1cc1cc4f96fc37
Dein <sadiandenniel@gmail.com>
2020-04-28 20:37:08 +0800

    Ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="cont">
-    <div>
-      <h1>Welcome to the Grading Sheet System</h1>
+    <div class="staff">
+      <h2>Are you one an admin or a teacher?</h2>
+      <div>
+        <nuxt-link to="/login" class="w3-button w3-round w3-green"
+          >Login</nuxt-link
+        >
+        <nuxt-link
+          to="/registration/check-code"
+          class="w3-button w3-round w3-blue"
+          >Register</nuxt-link
+        >
+      </div>
+    </div>
+    <div class="parent">
+      <h2>Do you want to view your child's crad?</h2>
+      <div>
+        <nuxt-link to="/cards" class="w3-button w3-round w3-blue"
+          >View Cards</nuxt-link
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -11,6 +29,32 @@
   height: 100%;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: auto 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: 'staff parent';
+}
+
+.head {
+  grid-area: head;
+  background: #4caf50;
+  color: white;
+  padding: 32px 0px;
+}
+
+.staff {
+  grid-area: staff;
+  background: #f1f1f1;
+}
+
+.parent {
+  grid-area: parent;
+  background: #ddffdd;
+}
+
+.cont > div {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
