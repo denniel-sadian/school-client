955f0c1cf8f19c14f71b54c479bc64e8663a20b6
Dein <sadiandenniel@gmail.com>
2020-04-30 18:27:19 +0800

    Ok

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,5 +23,6 @@ body,
 
 body {
   margin: 0px;
+  padding: 0px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
-    <header></header>
+  <div class="cont">
+    <div class="staff">
+      <h1 class="text">
+        STAFF
+      </h1>
+    </div>
+    <div class="parent">
+      <h1 class="text">
+        GUARDIAN
+      </h1>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cont {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+  grid-template-areas: 'staff parent';
+  background-size: cover;
+  background: url('/school.png') center no-repeat;
+}
+
+.staff {
+  grid-area: staff;
+}
+
+.parent {
+  grid-area: parent;
+}
+
+.cont > div {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
