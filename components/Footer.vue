8feaec1ace3fd4c1da94d2680a40041426cfe8b3
Dein <sadiandenniel@gmail.com>
2020-05-01 22:40:31 +0800

    Create a new component

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="w3-container">
+    <div class="w3-content w3-center">
+      <p>
+        <a href="https://sadian.herokuapp.com" class="w3-text-blue">SADIAN</a>
+        <i class="far fa-copyright"></i> 2020 Online Grading System
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+footer {
+  background: #ebf7e3;
+}
+</style>
