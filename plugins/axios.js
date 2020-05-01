abf50a3de85646a71e9028dff0b5995b8328fe81
Dein <sadiandenniel@gmail.com>
2020-05-01 20:07:20 +0800

    Add this interceptor

--- plugins/axios.js
+++ plugins/axios.js
@@ -5,4 +5,10 @@ export default function({ $axios, redirect }) {
   $axios.onRequest((config) => {
     console.log('Making request to ' + config.url)
   })
+
+  $axios.onError((error) => {
+    if (error.response.status === 401) {
+      redirect('/login')
+    }
+  })
 }
