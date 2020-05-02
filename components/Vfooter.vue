85d664ece025e6ecbb2a2b0740c44686f5aee331
Dein <sadiandenniel@gmail.com>
2020-05-02 19:42:07 +0800

    Update the footer

--- components/Vfooter.vue
+++ components/Vfooter.vue
@@ -2,9 +2,11 @@
   <footer class="w3-container">
     <div class="w3-content w3-center">
       <p>
-        <a href="https://sadian.herokuapp.com" class="w3-text-blue">SADIAN</a>
-        <i class="far fa-copyright"></i> 2020 Online Grading System
+        All of
+        <a href="https://sadian.herokuapp.com" class="w3-text-pink">Sadian</a>'s
+        backpain.
       </p>
+      <p><i class="far fa-copyright"></i> 2020 Online Grading System</p>
     </div>
   </footer>
 </template>
@@ -16,5 +18,6 @@ export default {}
 <style scoped>
 footer {
   background: #ebf7e3;
+  padding: 16px 0px;
 }
 </style>
