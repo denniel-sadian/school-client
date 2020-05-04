c1d13961022d57178a4be15c54b3250b365b125b
Dein <sadiandenniel@gmail.com>
2020-05-04 19:10:51 +0800

    Update this store

--- store/registration.js
+++ store/registration.js
@@ -1,10 +1,25 @@
 export const state = () => ({
-  credentials: null
+  credentials: null,
+  permissions: []
 })
 
 export const mutations = {
   SET_CREDENTIALS(state, cred) {
     state.credentials = cred
+  },
+  SET_PERMS(state, perms) {
+    state.permissions = perms
+  },
+  UPDATE_PERM(state, perm) {
+    state.permissions.filter((e) => e.id === perm.id)[0].section = perm.section
+    state.permissions.filter((e) => e.id === perm.id)[0].code = perm.code
+  },
+  ADD_PERM(state, perm) {
+    state.permissions.push(perm)
+    state.permissions = state.permissions.sort((a, b) => b.id - a.id)
+  },
+  DELETE_PERM(state, url) {
+    state.permissions = state.permissions.filter((e) => e.url !== url)
   }
 }
 
@@ -18,5 +33,17 @@ export const actions = {
       .catch(() => {
         commit('SET_CREDENTIALS', null)
       })
+  },
+  retrievePerms({ commit }) {
+    // Get all the permissions
+    return this.$axios
+      .get('accounts/student-permissions/')
+      .then(({ data }) => commit('SET_PERMS', data))
+  },
+  createPerm({ commit }, payload) {
+    // Create a permission
+    return this.$axios
+      .post('accounts/student-permissions/', payload)
+      .then(({ data }) => commit('ADD_PERM', data))
   }
 }
