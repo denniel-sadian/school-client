5c892a5ec05bff7a4582c4c803937855ac61c50e
Dein <sadiandenniel@gmail.com>
2020-05-06 13:20:31 +0800

    Add this condition

--- middleware/hasPermissionToRegister.js
+++ middleware/hasPermissionToRegister.js
@@ -1,6 +1,9 @@
 export default function({ store, redirect }) {
-  // Check if there's the token
   if (!store.state.registration.credentials) {
     return redirect('/registration/check-code')
+  }
+
+  if (!store.state.registration.notRegisteredStudents) {
+    return redirect('/registration/check-section-code')
   }
 }
