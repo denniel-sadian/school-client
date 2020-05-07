944170d32806420983a5e84d89d52530bf4d5c52
Dein <sadiandenniel@gmail.com>
2020-05-07 12:59:14 +0800

    Create this page

---
+++ pages/dashboard/exams/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <header class="w3-container">
+      <div class="w3-content w3-padding w3-center">
+        <h1>Exams</h1>
+        <p>
+          Both teachers and admins can view this page, but only the teachers can
+          deal with the exams.
+        </p>
+        <p>{{ teacherUrl }}</p>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      got: 0
+    }
+  },
+  computed: {
+    isTeacher() {
+      return this.$store.state.user.user.profile.role === 'teacher'
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style scoped>
+header {
+  margin-top: 60px;
+  padding: 64px 0px !important;
+  text-align: center;
+}
+
+header h1 {
+  text-transform: capitalize;
+}
+
+.w3-content {
+  max-width: 600px;
+}
+</style>
