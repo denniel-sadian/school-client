e838f439edc45761fe74a7e54a5144432b9cdecf
Dein <sadiandenniel@gmail.com>
2020-05-08 19:04:11 +0800

    Create this new component

---
+++ components/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cont">
+    {{ item }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    itemUrl: String
+  },
+  data() {
+    return {
+      item: {}
+    }
+  },
+  async created() {
+    await this.$axios.get(this.itemUrl).then(({ data }) => (this.item = data))
+  }
+}
+</script>
+
+<style scoped>
+.cont {
+  margin-bottom: 64px;
+  border: 2px solid black;
+  border-radius: 4px;
+  padding: 8px;
+}
+</style>
