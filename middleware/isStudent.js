4c97e8e0074caaa8eeb8d23b605798a34d091576
Dein <sadiandenniel@gmail.com>
2020-05-09 19:40:17 +0800

    Create this new middleware

---
+++ middleware/isStudent.js
@@ -0,0 +1,8 @@
+export default function({ store, redirect }) {
+  // Check if there's the token
+  if (store.state.user.user.user.student === null) {
+    return redirect({
+      name: 'dashboard'
+    })
+  }
+}
