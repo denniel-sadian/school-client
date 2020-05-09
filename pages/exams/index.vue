c88812b52ec2fd9399e22c0e6063e04108e98e15
Dein <sadiandenniel@gmail.com>
2020-05-09 15:54:56 +0800

    Update this page

--- pages/exams/index.vue
+++ pages/exams/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div><h1>HEllp</h1></div>
+  <div class="cont"></div>
 </template>
 
 <script>
@@ -8,4 +8,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.cont {
+  margin-top: 60px;
+}
+</style>
