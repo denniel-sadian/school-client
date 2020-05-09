34bb7a9a4cc53058f1c0fbe940fad61d20139acb
Dein <sadiandenniel@gmail.com>
2020-05-09 21:56:13 +0800

    Ok

--- components/ItemAnswering.vue
+++ components/ItemAnswering.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="item">
     <h4>{{ item.question }}</h4>
+    <label class="container" v-for="c in item.choices" :key="c.url">
+      <span>{{ c.text }}</span>
+      <input v-model="answer" :value="c.letter" type="radio" />
+      <span class="checkmark"></span>
+    </label>
   </div>
 </template>
 
@@ -8,6 +13,11 @@
 export default {
   props: {
     item: Object
+  },
+  data() {
+    return {
+      answer: ''
+    }
   }
 }
 </script>
@@ -15,5 +25,9 @@ export default {
 <style scoped>
 .item {
   margin: 50px 0px;
+}
+
+.checkmark {
+  border-radius: 32px;
 }
 </style>
