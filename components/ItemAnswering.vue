c8b73ef4a638032171f1366274c591ab4c6ec685
Dein <sadiandenniel@gmail.com>
2020-05-09 21:35:40 +0800

    Create a new component

---
+++ components/ItemAnswering.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="item">
+    <h4>{{ item.question }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  }
+}
+</script>
+
+<style scoped>
+.item {
+  margin: 50px 0px;
+}
+</style>
