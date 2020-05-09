683439e9ee87dfc9342c7441f66f3d8663f3fecb
Dein <sadiandenniel@gmail.com>
2020-05-09 15:32:26 +0800

    Remove these

--- plugins/getAuth.js
+++ plugins/getAuth.js
@@ -1,8 +1,8 @@
-export default async function({ $axios, app }) {
+export default function({ $axios, app }) {
   if (
     localStorage.getItem('school_access_token') !== null &&
     localStorage.getItem('school_refresh_token') !== null
   ) {
-    return await app.store.dispatch('user/getUser')
+    return app.store.dispatch('user/getUser')
   }
 }
