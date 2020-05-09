29dc0575ad291d62afd7cb6bb561e2cd7bd43040
Dein <sadiandenniel@gmail.com>
2020-05-09 14:26:54 +0800

    Create a new plugin

---
+++ plugins/getAuth.js
@@ -0,0 +1,8 @@
+export default async function({ $axios, app }) {
+  if (
+    localStorage.getItem('school_access_token') === null &&
+    localStorage.getItem('school_refresh_token') === null
+  ) {
+    return await app.store.dispatch('user/getUser')
+  }
+}
