f9653b8098db728267cf1e5fdb6bf4b35f116022
Dein <sadiandenniel@gmail.com>
2020-05-10 21:00:32 +0800

    Remore this condition

--- plugins/refresh_token.js
+++ plugins/refresh_token.js
@@ -1,22 +1,16 @@
 export default async function({ $axios, store }) {
   const refresher = async () => {
-    if (store.state.user.canRefreshToken) {
-      const rToken = localStorage.getItem('school_refresh_token')
+    const rToken = localStorage.getItem('school_refresh_token')
 
-      if (rToken === null) return
+    if (rToken === null) return
 
-      await $axios
-        .post(
-          'accounts/refresh-token/',
-          { refresh: rToken },
-          { progress: false }
-        )
-        .then((res) => {
-          localStorage.setItem('school_access_token', res.data.access)
-          $axios.setToken(res.data.access, 'Bearer')
-        })
-    }
+    await $axios
+      .post('accounts/refresh-token/', { refresh: rToken }, { progress: false })
+      .then((res) => {
+        localStorage.setItem('school_access_token', res.data.access)
+        $axios.setToken(res.data.access, 'Bearer')
+      })
   }
   await refresher()
-  setInterval(refresher, 10000)
+  setInterval(refresher, 30000)
 }
