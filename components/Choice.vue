3a67e5ebf5e75c1ba9b4d39b90938aa855c0d03a
Dein <sadiandenniel@gmail.com>
2020-05-11 15:33:54 +0800

    Remove this

--- components/Choice.vue
+++ components/Choice.vue
@@ -61,12 +61,6 @@ export default {
         this.updatingChoice = false
       })
     }
-  },
-  async mounted() {
-    await this.$axios
-      .get(this.choiceUrl)
-      .then(({ data }) => (this.choice = data))
-    this.doneLoading = true
   }
 }
 </script>
