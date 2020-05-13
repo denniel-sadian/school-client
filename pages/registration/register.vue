1a70f718db89a116dece20151d9a0e9d41df68c7
Dein <sadiandenniel@gmail.com>
2020-05-13 19:32:58 +0800

    Change the link

--- pages/registration/register.vue
+++ pages/registration/register.vue
@@ -145,7 +145,7 @@ export default {
                 password: this.password
               })
               .then(() => {
-                this.$router.push('/dashboard')
+                this.$router.push('/registration/sent')
               })
           })
           .catch(() => {
