4eee3f03ac0e9ba16174b267f2f304046d3109a0
Dein <sadiandenniel@gmail.com>
2020-06-01 22:56:54 +0800

    Change this

--- middleware/isSummaryEmpty.js
+++ middleware/isSummaryEmpty.js
@@ -1,5 +1,5 @@
 export default function({ store, redirect }) {
-  if (store.state.grading.summary === []) {
+  if (store.state.grading.summary.length === 0) {
     return redirect('/dashboard/sheets')
   }
 }
