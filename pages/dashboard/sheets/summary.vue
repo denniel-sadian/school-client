db40cb3fa12d148e35fbe9395d845de2e25b9344
Dein <sadiandenniel@gmail.com>
2020-06-01 23:39:59 +0800

    Make this file like this for now

--- pages/dashboard/sheets/summary.vue
+++ pages/dashboard/sheets/summary.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    {{ summary }}
+  <div class="page">
+    <header>
+      <h1>Summary of Quarterly Grades</h1>
+      <p>{{ subject }} of {{ section }}</p>
+    </header>
   </div>
 </template>
 
@@ -10,9 +13,21 @@ export default {
   computed: {
     summary() {
       return this.$store.state.grading.summary
+    },
+    section() {
+      return this.summary[0].section
+    },
+    subject() {
+      return this.summary[0].subject
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+header {
+  margin-top: 60px;
+  padding: 64px 8px;
+  text-align: center;
+}
+</style>
