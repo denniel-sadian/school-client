a3b3bd377336a90fa7b9a9e03c2df6de75dd0735
Dein <sadiandenniel@gmail.com>
2020-06-01 22:52:22 +0800

    Modify this

---
+++ pages/dashboard/sheets/summary.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ summary }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    summary() {
+      return this.$store.state.grading.summary
+    }
+  }
+}
+</script>
+
+<style scoped></style>
