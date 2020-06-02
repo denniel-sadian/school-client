2183681dba5bf752fc781fd5a234a09d556f0c8d
Dein <sadiandenniel@gmail.com>
2020-06-02 10:10:54 +0800

    Update this page

--- pages/dashboard/sheets/summary.vue
+++ pages/dashboard/sheets/summary.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="page">
+  <div>
     <header>
       <h1>Summary of Quarterly Grades</h1>
       <p>{{ subject }} of {{ section }}</p>
     </header>
+    <article>
+      <div class="table-cont">
+        <table>
+          <tr>
+            <th>Learners' Name</th>
+            <th>1st Quarter</th>
+            <th>2nd Quarter</th>
+            <th>3rd Quarter</th>
+            <th>4th Quarter</th>
+            <th>FINAL GRADE</th>
+            <th>REMARK</th>
+          </tr>
+        </table>
+      </div>
+    </article>
   </div>
 </template>
 
@@ -30,5 +45,20 @@ header {
   margin-top: 60px;
   padding: 64px 8px;
   text-align: center;
+}
+
+.table-cont {
+  overflow-x: auto;
+}
+
+table {
+  width: 100%;
+}
+
+th,
+td {
+  border: 1px solid black;
+  white-space: nowrap;
+  padding: 4px 8px;
 }
 </style>
