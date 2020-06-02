b33d60a6505340629403964455a92511ac6f94b1
Dein <sadiandenniel@gmail.com>
2020-06-02 10:14:32 +0800

    Create a new component

---
+++ components/SummaryRow.vue
@@ -0,0 +1,25 @@
+<template>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    record: Object,
+    kind: {
+      type: String,
+      default: 'jhs'
+    }
+  }
+}
+</script>
+
+<style></style>
